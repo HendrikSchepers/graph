a7c086dd7c06a5843f79c5ea00c3e6c3e4debdc4
hendrik <hetosche@freenet.de>
2025-04-29 13:34:33 +0200

    Anfang vom SchulGraph

--- src/schulgraph.java
+++ src/schulgraph.java
@@ -22,6 +22,43 @@ public class schulgraph {
         schulgraph.addVertex(Kunstraum);
         schulgraph.addEdge(new Edge(Kunstraum, Aula, 71));
         schulgraph.addEdge(new Edge(Informatikraum, Kunstraum, 64));
-        }
+        Vertex Musikraum = new Vertex( "Musikraum");
+        schulgraph.addVertex(Musikraum);
+        schulgraph.addEdge(new Edge(Informatikraum, Musikraum, 50));
+        schulgraph.addEdge(new Edge(Musikraum, Kunstraum, 9));
+        Vertex Informatikraum2 = new Vertex( "Inforaum2");
+        schulgraph.addVertex(Informatikraum2);
+        schulgraph.addEdge(new Edge(Musikraum, Informatikraum2, 55));
+        schulgraph.addEdge(new Edge(Informatikraum, Kunstraum, 55));
+        schulgraph.addEdge(new Edge(Informatikraum, Informatikraum2, 5));
+        Vertex schulzoo= new Vertex( "schulzoo");
+        schulgraph.addVertex(schulzoo);
+        schulgraph.addEdge(new Edge(Informatikraum, schulzoo, 17));
+        Vertex Chemieraum= new Vertex( "Chemieraum");
+        schulgraph.addVertex(Chemieraum);
+        schulgraph.addEdge(new Edge(Chemieraum, schulzoo, 7));
+        Vertex Bioraum= new Vertex( "Bioraum");
+        schulgraph.addVertex(Bioraum);
+        schulgraph.addEdge(new Edge(Chemieraum, Bioraum, 4));
+        Vertex Treppenhaus= new Vertex( "TreppenhausNW");
+        schulgraph.addVertex(Treppenhaus);
+        schulgraph.addEdge(new Edge(Chemieraum, Treppenhaus, 22));
+        schulgraph.addEdge(new Edge(Treppenhaus, Bioraum, 21));
+        schulgraph.addEdge(new Edge(Informatikraum, Treppenhaus, 13));
+        Vertex Fussballplatz= new Vertex( "Fussballplatz");
+        schulgraph.addVertex(Fussballplatz);
+        schulgraph.addEdge(new Edge(Informatikraum, Fussballplatz, 34));
+        Vertex Sporthalle= new Vertex( "Sporthalle");
+        schulgraph.addVertex(Sporthalle);
+        schulgraph.addEdge(new Edge(Informatikraum, Sporthalle, 95));
+        schulgraph.addEdge(new Edge(Sporthalle, Fussballplatz, 38));
+        Vertex TorSF= new Vertex( "TorSF");
+        schulgraph.addVertex(TorSF);
+        schulgraph.addEdge(new Edge(Sporthalle, TorSF, 63));
+        schulgraph.addEdge(new Edge(TorSF, Fussballplatz, 34));
+        Vertex Tischtennisplatten= new Vertex( "Tischtennisplatten);
+        schulgraph.addVertex(Tischtennisplatten);
+
+    }
 
 }
